ecf8d07610b49ac7eae85fca6a38e685927743a0
Hasbang <hassanabangura1@gmail.com>
2022-12-21 13:16:46 +0000

    make blog startup page

--- employee/urls.py
+++ employee/urls.py
@@ -3,10 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.blog, name='blog'),
+    path('employee/', views.index, name='index'),
     path('create/', views.create ,name='create'),
     path('create/createData/', views.createData , name='createData'),
     path('delete/<int:id>', views.delete ,name='delete'),
     path('update/<int:id>', views.update ,name='update'),
     path('update/updateData/<int:id>', views.updateData , name='updateData'),
+
 ]
